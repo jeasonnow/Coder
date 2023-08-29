590e62b60d4a8ec76c69596da3623f6d9d4fbd8a
顾一峰 <guyf@seeyon.com>
2023-08-29 15:58:21 +0800

    ✨ First Alfred Workflow to open workspace folder quickly

---
+++ README.md
@@ -0,0 +1,15 @@
+<p align="center">
+    <img src="./icon.png" width="138"/>
+<h1 align="center">Alfred Code Workflow</h1>
+
+## 介绍
+使用 Code 快捷命令，快速打开你工程目录中的文件夹。
+
+<kbd>
+  <img src="./resource/example.gif" width="100%">
+</kbd>
+
+## 初始化
+<kbd>
+  <img src="./resource/configure.jpg" width="100%">
+</kbd>
---
+++ rollup.config.mjs
@@ -0,0 +1,25 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
+import copy from 'rollup-plugin-copy';
+
+
+export default {
+  input: 'index.js',
+  plugins: [
+    resolve(),
+		commonjs(),
+    json(),
+    copy({
+      targets: [
+        {src: 'icon.png', dest: 'dist'},
+        {src: 'info.plist', dest: 'dist'},
+        {src: 'prefs.plist', dest: 'dist'},
+      ]
+    })
+  ],
+  output: {
+    format: 'cjs',
+    file: 'dist/index.js',
+  }
+}
